776f4591cfa1ea407fc80c1eab7abc9ac44c0573
kakts <akutsukeita@gmail.com>
2016-02-23 01:02:15 +0900

    initial

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
 function createWindow () {
+  console.log('----create window');
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
@@ -22,6 +23,7 @@ function createWindow () {
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
+    console.log('window has been closed');
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
